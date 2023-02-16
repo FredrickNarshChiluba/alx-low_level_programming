40b8f5d9ba1fdfc0edb0ea15f632f8d1fd956de6
FredrickNarshChiluba <chilubafredricknarsh@gmail.com>
2023-02-16 13:38:51 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,11 @@
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19
-			\n");
+	char a[] = "and that piece of art is useful\"";
+
+	char b[] = " - Dora Korpa, 2015-10-19\n";
+
+	int s = strlen(strcat(a , b));
 
 	write(2,
 	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
